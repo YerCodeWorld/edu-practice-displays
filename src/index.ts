ccca66755bc4d81ae8faa5eb3f3874a5d2684df4
YerCodeWorld <yahiradolfo37@gmail.com>
2025-12-13 00:41:26 -0400

    Added single object export proposal on comments

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+// 
+// import * from "./quizzes"
+
+// export const Contracts: ContractType[] = {
+//     // add all the contracts here, instead of exporting all of them individually    
+// }
+
 export * from "./quizzes";
 export * from "./elements";
 export * from "./services";
